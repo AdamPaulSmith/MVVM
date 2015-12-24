df41ee8a81e57623f514858dffb4417d367c40c8
Adam Smith <github@therx.co.uk>
2015-12-24 21:30:29 +0000

    Complete Command and View model abstract classes with their associated unit tests.

---
+++ Code/TheRX.MVVM.VisualSupport.Tests.Unit/CommandTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace TheRX.MVVM.VisualSupport.Tests.Unit
+{
+    [TestFixture]
+    public class CommandTests
+    {
+        [Test]
+        public void CanExecute_WhenCommandIsContructedWithNoParameters_ReturnsTrue()
+        {
+            var testCommand = new TestCommand();
+
+            bool commandCanExecute = testCommand.CanExecute(null);
+
+            commandCanExecute.ShouldBeTrue();
+        }
+
+        [Test]
+        public void CanExecute_WhenTheCommandIsContructedWithTheCanExecuteParamterSet_ReturnsTheSetValue([Values(true, false)] bool canExecuteParameter)
+        {
+            var testCommand = new TestCommand(canExecuteParameter);
+
+            bool commandCanExecute = testCommand.CanExecute(null);
+
+            commandCanExecute.ShouldBe(canExecuteParameter);
+        }
+
+        [Test]
+        public void CanExecuteChanged_IsRaised_WhenSetCanExecuteIsInvoked()
+        {
+            bool canExecuteChangedInvoked = false;
+
+            var testCommand = new TestCommand();
+            testCommand.CanExecuteChanged += (sender, args) => canExecuteChangedInvoked = true;
+
+            testCommand.InvokeSetCanExecuteChanged();
+
+            canExecuteChangedInvoked.ShouldBeTrue();
+        }
+
+        private class TestCommand : Command
+        {
+            public TestCommand()
+            {
+            }
+
+            public TestCommand(bool canExecute) : base(canExecute)
+            {
+            }
+
+            public override void Execute(object parameter)
+            {
+            }
+
+            public void InvokeSetCanExecuteChanged()
+            {
+                SetCanExecute(true);
+            }
+        }
+    }
+}
---
+++ Code/TheRX.MVVM.VisualSupport.Tests.Unit/ViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace TheRX.MVVM.VisualSupport.Tests.Unit
+{
+    [TestFixture]
+    public class ViewModelTests
+    {
+        [Test]
+        public void PropertyChanged_IsInvokedWithTheCorrectPropertyName_WhenInvokedInAPropertySetter()
+        {
+            string propertyName = string.Empty;
+
+            var testViewModel = new TestViewModel();
+            testViewModel.PropertyChanged += (sender, args) => propertyName = args.PropertyName;
+            testViewModel.TestProperty = "New value";
+
+            propertyName.ShouldBeSameAs(nameof(testViewModel.TestProperty));
+        }
+
+        private class TestViewModel : ViewModel
+        {
+            private string _testProperty;
+
+            public string TestProperty
+            {
+                get { return _testProperty; }
+                set
+                {
+                    _testProperty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
